b89b486f1dbc9321bcf92522661c62d369f64e68
Egor Kutovoy <egorkincbl@gmail.com>
2023-04-15 18:04:45 +0300

    Fix create_db.sh

--- scripts/create_db.sh
+++ scripts/create_db.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-psql -U postgres -d postgres -c "\copy tasks FROM 'data/tasks.csv' with (format csv, header true, delimiter ',')"
+psql -h localhost -U postgres -d postgres -c "\copy tasks FROM 'data/tasks.csv' with (format csv, header true, delimiter ',')"
